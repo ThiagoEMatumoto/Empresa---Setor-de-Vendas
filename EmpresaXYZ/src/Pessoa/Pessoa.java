168028bf8e2c1dcf5b9302437eab347260986ffe
Vitor-devs <vitorcoelho2004@outlook.com>
2022-09-26 21:37:29 -0300

    building_pessoa

--- EmpresaXYZ/src/Pessoa/Pessoa.java
+++ EmpresaXYZ/src/Pessoa/Pessoa.java
@@ -8,4 +8,20 @@ abstract class Pessoa {
 		this.nome = nome;
 		this.telefone = telefone;
 	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+
+
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+	public int getTelefone() {
+		return telefone;
+	}
 }
